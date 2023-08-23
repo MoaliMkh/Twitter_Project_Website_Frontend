f345e841e1b39cd75c2e395dc8416d3fdd61f9f9
MohammadAli MohammadKhani <mohammadali1379@gmail.com>
2023-08-23 09:07:44 +0330

    revert wrong commit

--- src/App.js
+++ src/App.js
@@ -1,225 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import Chart from 'chart.js/auto';
-import { useState, useEffect } from "react";
-import axios from 'axios';
+import Topics from './Topics';
+import Home from './Home'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { registerLicense } from '@syncfusion/ej2-base';
+import ChartResultsAfterTopics from './ChartResultsAfterTopics';
+import ChartSection from './ChartSection';
+
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2V1hiQlRPf0BDX3xLflF1VWZTflZ6cFRWESFaRnZdQV1rSH1Tc0ZjXHxccHFX');
 
 
 function App() {
 
-  const getResponse = async (username) => {
-    const response = await axios.get('http://127.0.0.1:8000/user/', {
-      params: {username: username }
-    });
-    console.log(response.data)
-  }
-
-  useEffect(() => {
-    // code to run after render goes here
-    loadChart('Tweets', [12, 19, 3, 5, 2])
-    loadChart('Images', [3, 4, 3, 4, 3])
-    loadChart('Friends', [4, 5, 8, 3, 2])
-    getResponse('SElahimanesh')
-  });
-
-  const loadChart = (id, data) => {
-    const ctx = document.getElementById(id);
-  
-    new Chart(ctx, {
-      type: 'doughnut',
-      data: {
-        labels: ['Angry', 'Sad', 'Neutral', 'Joy/Happy', 'Surprise/Emotional'],
-        datasets: [{
-          label: '# of Votes',
-          data: data,
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    });
-  }
-
-  // loadChart()
-
   return (
     <div className="App">
-
-    <div className='header-box'>
-      <h3>How Much Similarity Do You Have in The Real-World with Your Characteristics in Social Media?</h3>
-      <hr />    
-    </div>
-
-      <div className='top-container'>
-
-        <div className='sentiment-container'>
-
-        <div className='pie-chart'>
-          <canvas id="Tweets"></canvas>
-        </div>
-
-        <div className='pie-chart'>
-          <canvas id="Images"></canvas>
-        </div>
-
-        <div className='pie-chart'>
-          <canvas id="Friends"></canvas>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-        </div>
-
-        <div className="task-progress">
-          <p>How much similar are you in the real-world similar to your tweets?
-          <span>20/3-0</span>
-          </p>
-          <progress className="progress progress--task3" max="100" value="20"></progress>
-        </div>
-
-        <div className="task-progress">
-          <p>How much similar are you in the real-world similar to your images?
-          <span>20/3-0</span>
-          </p>
-          <progress className="progress progress--task4" max="100" value="40"></progress>
-        </div>
-      </div>
-     
-
-      
- 
-      <div className='bottom-container'>
-        <div className='topics'>
-
-            <h3>Topics of Your Tweets</h3>
-            <hr />
-            <div className="task-progress-2">
-              <p>Politics
-              </p>
-              <progress className="progress-2 progress--task1" max="100" value="40"></progress>
-            </div>
-
-            <div className="task-progress-2">
-              <p>Social
-              </p>
-              <progress className="progress-2 progress--task2" max="100" value="40"></progress>
-            </div>
-
-            <div className="task-progress-2">
-              <p>Social
-              </p>
-              <progress className="progress-2 progress--task3" max="100" value="40"></progress>
-            </div>
-
-            <div className="task-progress-2">
-              <p>Social
-              </p>
-              <progress className="progress-2 progress--task4" max="100" value="40"></progress>
-            </div>
-
-            <div className="task-progress-2">
-              <p>Social
-              </p>
-              <progress className="progress-2 progress--task1" max="100" value="40"></progress>
-            </div>
-
-            <div className="task-progress-2">
-              <p>Social
-              </p>
-              <progress className="progress-2 progress--task2" max="100" value="40"></progress>
-            </div>
-
-            <div className="task-progress-2">
-              <p>Social
-              </p>
-              <progress className="progress-2 progress--task3" max="100" value="40"></progress>
-            </div>
-
-            <div className="task-progress-2">
-              <p>Social
-              </p>
-              <progress className="progress-2 progress--task4" max="100" value="40"></progress>
-            </div>
-
-        </div>
-
-        <div className='topics'>
-
-<h3>Topics of Your Media Images</h3>
-<hr />
-<div className="task-progress-2">
-  <p>Politics
-  </p>
-  <progress className="progress-2 progress--task1" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task2" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task3" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task4" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task1" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task2" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task3" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task4" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task4" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task4" max="100" value="40"></progress>
-</div>
-
-<div className="task-progress-2">
-  <p>Social
-  </p>
-  <progress className="progress-2 progress--task4" max="100" value="40"></progress>
-</div>
-
-</div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="topics" element={<Topics />} />
+      <Route path="newcharts" element={<ChartSection />} />
+      <Route path="charts" element={<ChartResultsAfterTopics />} />
+    </Routes>
+  </div>
   );
 }
 
